38d6ce0c7d94b9a22939606cfd90d6391044f894
HunterX-7 <fraquin7@gmail.com>
2023-06-08 12:57:56 -0500

    Update the styles.js change font weight and color

--- src/styles.js
+++ src/styles.js
@@ -9,9 +9,9 @@ const styles = {
       "text-white font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2",
   
     sectionHeadText:
-      "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+      "font-extrabold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
-      "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+      "sm:text-[18px] text-[14px] tracking-wider",
   };
   
   export default styles;
