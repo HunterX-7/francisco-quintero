22066a94bc81734e9a2b83b7f7a72c7b1ff8f742
HunterX-7 <fraquin7@gmail.com>
2023-06-14 11:54:21 -0500

    Update the hero text style

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,7 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
     heroHeadText:
-      "font-black text-white lg:text-[70px] sm:text-[54px] xs:text-[40px] text-[36px] lg:leading-[98px] mt-2",
+      "font-extrabold text-white lg:text-[70px] sm:text-[54px] xs:text-[40px] text-[36px] lg:leading-[98px] mt-2",
     heroSubText:
       "text-white font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2",
   
